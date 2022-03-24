2189c2d763acb4f0280c48d61724682a4d9772be
PTRL480\Vyshnavi.Gubala <vyshnavi.m@yesgnome.com>
2022-03-24 16:59:03 +0530

    RandomPlatform Spawning is initialized.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -7,21 +7,38 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     public PlayerMovement player;
    public Camera cam;
-    
+    public GameObject[] platformPrefab;
+   public  Vector3 offSet;
+    public float spawnPoint=0;
+   // public GameObject currentPlatform;
+    public float safeMargin;
     void Start()
     {
-
         
+      //currentPlatform=  Instantiate(platformPrefab[0], transform.position+offSet,Quaternion.identity);
+       // GameObject tempBlock= Instantiate(platformPrefab[k],transform.position+new Vector3(5f,0,0),Quaternion.identity);
+       
+      
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
        
         if(player!=null)
         {
             cam.transform.position = new Vector3(player.transform.position.x, cam.transform.position.y, cam.transform.position.z);
         }
-        
+        while(spawnPoint<player.transform.position.x+safeMargin)
+        {
+            int k = Random.Range(0, platformPrefab.Length);
+           GameObject currentPlatform = Instantiate(platformPrefab[k], transform.position , Quaternion.identity);
+            PlatformController platform= currentPlatform.GetComponent<PlatformController>();
+            currentPlatform.transform.position = new Vector3(spawnPoint + platform.platformSize / 2, 0, 0);
+            spawnPoint = 35f;
+
+        }
     }
 }
---
+++ Assets/PlatformController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float platformSize;
+    void Start()
+    {
+        platformSize = gameObject.transform.localScale.x;
+        Debug.Log(platformSize);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
